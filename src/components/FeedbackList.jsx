2220c0b11e055a6ec6812334e113d7f024e463e0
sikderweb <sikderweb@gmail.com>
2021-12-18 22:02:21 +0600

    first commit

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,16 @@
+import FeedbackItem from "./FeedbackItem"
+const FeedbackList = ({ feedback }) => {
+  if (!feedback || feedback.length === 0) {
+    return <p>There is no feedback yet</p>
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item)=>( 
+       <FeedbackItem key = {item.id} item = {item} />  
+      ))}
+    </div>
+  )
+}
+
+export default FeedbackList
+
